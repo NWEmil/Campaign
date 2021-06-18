95772c998006a866d16de7f2f970f6b1bade558f
Wulfvenstierna <emil@nwrp.eu>
2021-06-18 19:17:11 +0200

    public edition

---
+++ factions.php
@@ -0,0 +1,17 @@
+ <?php 
+$url = 'http://129.146.80.62:3000/api/v1/factions';
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$data = curl_exec($ch);
+curl_close($ch);
+/*
+header('Content-Type: application/json');
+header('Expires: Sun, 01 Jan 2014 00:00:00 GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', FALSE);
+header('Pragma: no-cache');
+*/
+echo $data;
+ ?> 
